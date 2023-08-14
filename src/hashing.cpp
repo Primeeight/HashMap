e698cc5c672bffe1a57e56dc014024c852d14611
Primeeight <zacharywray@gmail.com>
2023-08-14 19:00:41 -0400

    8/13/23

--- src/hashing.cpp
+++ src/hashing.cpp
@@ -1,4 +1,4 @@
-//Demopnstrates a HashTable and operations.
+//Demonstrates a HashTable and operations.
 
 #include <optional>
 #include <iostream>
@@ -10,12 +10,15 @@
 using namespace std;
 
 /**
- * TODO
- * Create a hash entry class with an vector of sequences
- * Consider entering the data fields for the airport here.
- */
+   * Open record that includes constructor.
+   */
 class HashEntry {
-private:
+
+public:
+    HashEntry() = default;
+
+    HashEntry(optional<HashEntry> Optional) {}
+
     std::string code;
     std::string name;
     std::string city;
@@ -28,102 +31,35 @@ private:
     int longSec;
     std::string longDir;
     int altitude;
-
-
-
-    /**
-     * TODO
-     * May add remove, add, entry, get, and output sequence methods here.
-     * Constructor
-     */
-public:
-    HashEntry(int key, int value) {
-        this->latDegree = key;
-        this->latMin = value;
-
-    }
-
-    const string &getCode() const {
-        return code;
-    }
-
-    const string &getName() const {
-        return name;
-    }
-
-    const string &getCity() const {
-        return city;
-    }
-
-    const string &getCountry() const {
-        return country;
-    }
-
-    int getLatSec() const {
-        return latSec;
-    }
-
-    const string &getLatDir() const {
-        return latDir;
-    }
-
-    int getLongMin() const {
-        return longMin;
-    }
-
-    int getLongSec() const {
-        return longSec;
-    }
-
-    const string &getLongDir() const {
-        return longDir;
-    }
-
-    int getAltitude() const {
-        return altitude;
-    }
-
-
-    int getLatDegree() {
-        return latDegree;
-    }
-
-    int getLatMin() {
-        return latMin;
-    }
 };
 
-const int TABLE_SIZE = 128;
 
 class HashMap {
 private:
-//    HashEntry **table;
-    //Verify data type of sequence.
-    Sequence<HashEntry> entry;
-    vector<Sequence<HashEntry>> table;
 
+    vector<Sequence<HashEntry>> table;
 public:
-    /**TODO
-     * Verify Constructor
+    /**
+     * Default constructor
      */
     HashMap() = default;
 
     /**
+     * TODO
      *Traverse the vector for the hash.
      * Traverse the sequence for the specified code.
-     * If a hash entry with the specified code is found, return the hash entry
-     * otherwise, return -1.
+     * If a hash entry with the specified code is found, return the hash entry.
+     * Otherwise return an empty hash entry.
      * @param code
      * @return
      */
     optional<HashEntry> get(std::string code) {
         int hash = getHash(code);
         for (int i = 0; i < table[hash].length(); i++) {
-            if (table[hash].entry(i)->getCode() == code)
+            if (table[hash].entry(i)->code == code)
                 return table[hash].entry(i);
         }
-        //Will this be the proper return?
-        return nullopt;
+        return optional<HashEntry>();
     }
 
     /**
@@ -131,7 +67,7 @@ public:
      * @param code
      * @param HashEntry entry
      * Traverse the vector using the hash.
-     * At the specified sequence, use the add fucntion to add the Hash Entry.
+     * At the specified sequence, use the add function to add the hash entry to the end of the sequence.
      */
     void put(std::string code, HashEntry entry) {
         int hash = getHash(code);
@@ -139,25 +75,41 @@ public:
         table[hash].add(entry, size);
     }
 
-    void remove(std::string code, HashEntry entry) {
-
+    /**
+     * TODO
+     * get the hash
+     * define the size as the length of the current sequence.
+     * iterate through the sequence for the element to be removed.
+     * match the provided code with the element to be removed's code.
+     * remove the element at the position.
+     *
+     * Convert the optional data type for removal.
+     * @param code
+     */
+    void remove(std::string code) {
+        HashEntry currentHashEntry = *new HashEntry;
         int hash = getHash(code);
         int size = table[hash].length();
         for (int i = 0; i < size; i++) {
-            if (table[hash].entry(i) == entry)
-                table[hash].remove(entry, i);
+            if (table[hash].entry(i)->code == code) {
+                currentHashEntry = table[hash].entry(i);
+                table[hash].remove(currentHashEntry, i);
+            }
         }
     }
-
+    /**
+     * get the hash using polynomial hashing.
+     * @param code
+     * @return int
+     */
     int getHash(std::string code) {
         int v1;
         for (int i = 0; i < code.length(); i++)
             v1 = v1 + code[i];
-        return (v1 * 17) % TABLE_SIZE;
+        return (v1 * 17) % getSize();
     }
 
     /**
-     * TODO
      * iterate through the vector, adding the length of all sequences.
      * @return the size
      */
@@ -175,49 +127,52 @@ public:
      * while the end of the file has not been reached, read each line into the table based on their code.
      * @param filename
      */
-    void inputHashMap(string filename) {
+    void inputHashMap(std::string filename) {
         ifstream infile(filename);
-        HashEntry newEntry = new HashEntry;
-        if (!infile)
+        int dataSize;
+        HashEntry newEntry = *new HashEntry;
+        if (!infile) {
             cout << "unable to open the file";
-        exit(1);
+            exit(1);
+        }
+        if (infile.is_open()) {
+            infile >> dataSize;
+        }
         //Iterate through the file.
-        infile >> newEntry.code;
-        infile >> newEntry.name;
-        infile >> newEntry.city;
-        infile >> newEntry.country;
-        infile >> newEntry.latDegree;
-        infile >> newEntry.latMin;
-        infile >> newEntry.latSec;
-        infile >> newEntry.latDir;
-        infile >> newEntry.longMin;
-        infile >> newEntry.longSec;
-        infile >> newEntry.longDir;
-        infile >> newEntry.altitude;
-        put(newEntry.code, newEntry);
-
+        for (int i = 0; i < dataSize; i++) {
+            infile >> newEntry.code;
+            infile >> newEntry.name;
+            infile >> newEntry.city;
+            infile >> newEntry.country;
+            infile >> newEntry.latDegree;
+            infile >> newEntry.latMin;
+            infile >> newEntry.latSec;
+            infile >> newEntry.latDir;
+            infile >> newEntry.longMin;
+            infile >> newEntry.longSec;
+            infile >> newEntry.longDir;
+            infile >> newEntry.altitude;
+            put(newEntry.code, newEntry);
+        }
         //close the file.
         infile.close();
     }
+
 
     /**
      * TODO
      * iterate over the table.
      * For each sequence at an index, input all entries into a string stream.
      * output the string stream into a .txt file.
+     * if there is already an output file, override that file.
      */
     void outputHashMap() {
         std::stringstream ss;
-//        if(){
-//         //file exists
-//        }
         ofstream OutputFile("output.txt");
 
         for (int i = 0; i < table.size(); i++) {
             OutputFile << table[i].outputSequence();
-            //output the entry to the file.
         }
-        // may need to do final output to file
         OutputFile.close();
     }
 
@@ -225,17 +180,6 @@ public:
 };
 
 int main() {
-    HashMap hm;
-
-    hm.outputHashMap();
-    hm.getSize();
-    hm.get("YWG");
-    hm.get("CMB");
-    hm.get("HND");
-    hm.getSize();
-    hm.outputHashMap();
-    cout << "Value of latDegree = 3, is " << hm.get(3) << endl;
-
     /** Output the map */
 
     /** How many records are in this map data structure?*/
@@ -250,13 +194,14 @@ int main() {
 
     /** Output the map */
 
-//    hm.put(3, 23);
-//    hm.put(5, 2377);
-//    hm.put(9, 263);
-//    cout << "Value of latDegree = 3, is " << hm.get(3) << endl;
-//    hm.put(12, 230);
-//    cout << "override an existing entry of (3, 23) with (3,123)" << endl;
-//    hm.put(3, 123);
-//    cout << "New latMin of latDegree = 3, is " << hm.get(3) << endl;
+    HashMap hm;
+    hm.inputHashMap("air16.dat");
+    hm.outputHashMap();
+    hm.getSize();
+    hm.get("YWG");
+    hm.get("CMB");
+    hm.remove("HND");
+    hm.getSize();
+    hm.outputHashMap();
 }
 
